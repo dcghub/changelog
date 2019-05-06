cff46b58797a64ddcb78eb5c7138c9e4a01ebb80
Diego Calleja <diegocg@gmail.com>
2019-05-06 19:37:23 +0200

    fish-ize listmergesfromlinus.sh

--- listmergesfromlinus.sh
+++ listmergesfromlinus.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
-set -e
-if [[ $# -ne 2 ]]
-then
+#!/bin/fish
+if test (count $argv) != 3
 	echo "Not enough parameters"
 	exit 1
-fi
+end
 
-cd linux
+cd $argv[3]
 
-echo "List of Linus merges from $1 to $2"
-for i in $(git rev-list --author="Linus Torvalds" --merges --reverse $1..$2); do
-	line=$(git show --pretty=medium $i | sed -n 8p)
+echo "List of Linus merges from $argv[1] to $argv[2]"
+for i in (git rev-list --author="Linus Torvalds" --merges --reverse $argv[1]..$argv[2])
+	set line (git show --pretty=medium $i | sed -n 8p)
 	#echo $i, $line
-	subsys=$(echo $line | grep -oP "(?<=[Pull|Merge] ).*?(?= from)")
+	set subsys (echo $line | grep -oP "(?<=[Pull|Merge] ).*?(?= from)")
 	echo " * [[https://git.kernel.org/torvalds/c/$i|$subsys]]"
-done
+end
