0511511eff376a5b543dc7a7000ce47ac6bbee4e
Diego Calleja <diegocg@gmail.com>
2019-07-23 20:34:03 +0200

    List merge commit messages too

--- listmergesfromlinus.sh
+++ listmergesfromlinus.sh
@@ -6,10 +6,25 @@ end
 
 cd $argv[3]
 
-echo "List of Linus merges from $argv[1] to $argv[2]"
-for i in (git rev-list --author="Linus Torvalds" --merges --reverse $argv[1]..$argv[2])
-	set line (git show --pretty=medium $i | sed -n 8p)
+function parsecommitmsg
+	set line (git show --pretty=medium --no-patch $argv[1] | sed -n 8p)
 	#echo $i, $line
 	set subsys (echo $line | grep -oP "(?<=[Pull|Merge] ).*?(?= from)")
-	echo " * [[https://git.kernel.org/torvalds/c/$i|$subsys]]"
+	echo " * [[https://git.kernel.org/torvalds/c/$argv[1]|$subsys]]"
+end
+
+
+
+set list (git rev-list --author="Linus Torvalds" --merges --reverse $argv[1]..$argv[2])
+
+echo "List of Linus merges from $argv[1] to $argv[2]"
+for i in $list
+	parsecommitmsg $i
+end
+
+echo "Changelog of merges from $argv[1] to $argv[2]"
+
+for i in $list
+	parsecommitmsg $i
+	git show --pretty=medium --no-patch $i
 end
