034396a889c09fe2761450dc22e9a4f82e97b191
Diego Calleja <diegocg@gmail.com>
2019-09-16 20:08:12 +0200

    Linux 5.3 + some addons to older kernels

--- downloadfromkn.sh
+++ downloadfromkn.sh
@@ -9,7 +9,7 @@ set urllist LinuxVersions
 set urllist $urllist "Linux_2_6_"(seq 0 39)
 set urllist $urllist "Linux_3."(seq 0 19)
 set urllist $urllist "Linux_4."(seq 0 20)
-set urllist $urllist "Linux_5."(seq 0 2)
+set urllist $urllist "Linux_5."(seq 0 3)
 
 for url in $urllist
 	curl -s $baseurl$url$suffix > archive/$url
